a5b632f58f3a10f5b1102a72e8d591a8eee6a0f3
Tanvir <u1604090@student.cuet.ac.bd>
2022-12-07 01:21:54 +0600

    Some modification

--- src/NewsFeed.java
+++ src/NewsFeed.java
@@ -9,9 +9,6 @@ public class NewsFeed extends JFrame {
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(new Dimension(500,600));
-
-
-
     }
 
     public static void main(String[] args) {
